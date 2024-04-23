d3eca935b17c1e18402a8d847a6fb715826e3c37
Cole Stuedeman <94010928+Stuede1@users.noreply.github.com>
2024-04-22 21:07:26 -0500

    Update EntityAccessor.java
    EntityAccessor to force remove entities

--- Xplat/src/main/java/vazkii/botania/mixin/EntityAccessor.java
+++ Xplat/src/main/java/vazkii/botania/mixin/EntityAccessor.java
@@ -13,3 +13,11 @@ import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
+//EntityAccessor to force remove entities 
+
+@Mixin(Entity.class)
+public interface EntityAccessor {
+	@Invoker
+	void callUnsetRemoved();
+}
+
